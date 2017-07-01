f48aff5895082a880e8e4f7543e5b4754c69370e
jimmdd <jding@Jingluns-MacBook-Pro.local>
2017-07-01 15:35:38 -0500

    removed useless code and fixed modal open and close
    now modal open and close are controlled by state shared with all the
    component.

--- li-app/src/ImgBox.js
+++ li-app/src/ImgBox.js
@@ -1,111 +1,13 @@
 import React, { Component } from 'react';
-import ToggleButton from './ToggleButton';
-
 class ImgBox extends Component {
-  constructor() {
-    super();
-    this.state = {
-      value: "",
-    }
 
-    this._handleDescriptionChange = this._handleDescriptionChange.bind(this);
-    this._handleDescriptonSubmit = this._handleDescriptonSubmit.bind(this);
-    this._toggleForm = this._toggleForm.bind(this);
-    this._getDescriptions = this._getDescriptions.bind(this);
-  }
-
-  componentDidMount() {
-    this._getDescriptions();
-  }
-  //get photo descriptions from local storage
-  _getDescriptions() {
-    let des_key = "des_" + this.props.id;
-    let des_val = localStorage.getItem(des_key);
-    if (this.state.value === "" && des_val != null)
-      this.setState({ value: des_val });
-  }
-
-  //save photo descriptions from user input
-  _saveDescription() {
-    let des_data = this.state.value;
-    if (des_data === "") {
-      alert("Please enter your description!")
-    } else {
-      let des_key = "des_" + this.props.id;
-      localStorage.setItem(des_key, des_data);
-
-    }
-  }
-  _handleDescriptionChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  _handleDescriptonSubmit(event) {
-    event.preventDefault();
-    this._toggleForm();
-    this._saveDescription();
-  }
-
-
-
-
-  //TO-DO Use state rather than grab by id
-  _toggleForm() {
-    let display = document.getElementById(this.props.id).style.display
-    display === 'none' ?
-      display = 'block' :
-      display = 'none';
-  }
-
+//TO-DO delete ImgBOX class and just use Thumbnail
   render() {
     return (
-      // <div className="grid-20 mobile-grid-50">
-      //   <a type="button" onClick={this._handlePopClick}>
-      //     <img className="img-thumbnail"
-      //       alt={this.props.title}
-      //       src={this.props.thumbnailUrl} />
-      //   </a>
-      //   {/*{props.child}*/}
-
-      //   <div id={"modal"} className="container">
-      //     <span className="close" onClick={this._handlePopClose}>&times;</span>
-      //     <img className="modal-img img-responsive" id={this.props.id}
-      //       src={this.props.url} alt={this.props.title} />
-      //     <div id="title">
-      //       <h4 className="text-center" >{this.props.title}</h4>
-      //       <p className="text-center" >{this.state.value}</p>
-
-      //       <form onSubmit={this._handleDescriptonSubmit}>
-
-      //         <div className="form-group">
-      //           <div id="des-form">
-      //             <textarea className="form-control btn-block" rows="3"
-      //               placeholder="Write your Description Here"
-      //               value={this.state.value}
-      //               onChange={this._handleDescriptionChange}
-      //             />
-      //             <p>
-      //               <button type="button" className="btn btn-danger">Cancel</button>
-      //               <button type="submit"
-      //                 className="btn btn-success">Save</button>
-      //             </p>
-      //           </div>
-
-      //           {/*TO-DO pass state to the ToggleButton*/}
-      //           <ToggleButton class="btn btn-primary"
-      //             on="Add/Edit Description"
-      //             off="Save Description"
-      //             id="des-form" />
-
-
-      //         </div>
-      //       </form>
-      //     </div>
-      //   </div>
-      // </div>
       <Thumbnail title={this.props.title}
         thumbnailUrl={this.props.thumbnailUrl}
-        url = {this.props.url} />
+        url = {this.props.url} 
+        id = {this.props.id}/>
     );
   }
 }
@@ -127,17 +29,8 @@ class Thumbnail extends Component {
                     isClicked: true
                   });
   }
-
-  _renderModal() {
-    if (this.state.isClicked) {
-      return (
-        <Modal id={this.props.id}
-          title={this.props.title}
-          url={this.props.url}
-          style={this.state.style}
-        />
-      );
-    }
+  _closeModal(){
+    this.setState({isClicked: false});
   }
 
   render() {
@@ -148,7 +41,12 @@ class Thumbnail extends Component {
             alt={this.props.title}
             src={this.props.thumbnailUrl} />
         </a>
-        {this._renderModal()}
+        {this.state.isClicked? <Modal id={this.props.id}
+          title={this.props.title}
+          url={this.props.url}
+          style={this.state.style}
+          close = {this._closeModal.bind(this)}
+        /> : <div></div>}
       </div>
     );
   }
@@ -160,6 +58,7 @@ class Modal extends Component {
     this.state = {
       value: "",
       isOpen: true,
+      isToggleOn: true,
       style: { display: "none" }
     }
     this._handlePopClose = this._handlePopClose.bind(this);
@@ -180,16 +79,30 @@ class Modal extends Component {
       this.setState({ value: des_val });
   }
 
-  //TO-DO CHANGE STATE
-  _handlePopClose() {
+  _getDescriptionFromChild(v) {
+    this.setState({value: v});
+  }
+ _changeToggle(){
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
-    if (this.state.isOpen)
-      this.setState({ style: { display: "none" } });
+ }
+
+  //TO-DO CHANGE STATE
+  _handlePopClose() {
+    this.state.isOpen? 
+    this.props.close() :
+    this.setState(prevState => ({
+      isOpen: !prevState.isOpen
+    }));
+    // this.state.isOpen?
+    //   this.setState({ style: { display: "none" } }):
+    //   this.setState({ style: { display: "block" } });
+    
   }
 
   render() {
+    console.log(this.props.id);
     return (
       <div id={"modal"} className="container" style={this.state.style}>
         <span className="close" onClick={this._handlePopClose}>&times;</span>
@@ -200,21 +113,70 @@ class Modal extends Component {
           <p className="text-center" >{this.state.value}</p>
         </div>
         {this.props.children}
+        {this.state.isToggleOn? 
+        <DescriptionForm id = {this.props.id} des_get = {this._getDescriptionFromChild.bind(this)}/>
+        :<div>Add/Edit Description</div>}
+        
       </div>
-
     );
   }
 }
 
-function DescriptionFrom(props) {
+class DescriptionForm extends Component{
+  constructor(){
+    super();
+    this.state={
+        value:"",
+    }
+    this._handleDescriptionChange = this._handleDescriptionChange.bind(this);
+    this._handleDescriptonSubmit = this._handleDescriptonSubmit.bind(this);
+    this._saveDescription = this._saveDescription.bind(this);
+  }
+
+  componentDidMount(){
+      this._getDescriptions();
+  }
+  _handleDescriptionChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  _handleDescriptonSubmit(event) {
+    event.preventDefault();
+    //this._toggleForm();
+    this._saveDescription();
+  }
+
+   //get photo descriptions from local storage
+  _getDescriptions() {
+    let des_key = "des_" + this.props.id;
+    console.log(des_key);
+    let des_val = localStorage.getItem(des_key);
+    if (this.state.value === "" && des_val != null)
+      this.setState({ value: des_val });
+  }
+
+     //save photo descriptions from user input
+  _saveDescription() {
+    alert("saving in progress");
+    let des_data = this.state.value;
+    if (des_data === "") {
+      alert("Please enter your description!")
+    } else {
+      let des_key = "des_" + this.props.id;
+      localStorage.setItem(des_key, des_data);
+      this.props.des_get(des_data);
+      //TO-DO HIDE FORM AFTER SAVE
+    }
+  }
+
+  render(){
   return (
-    <form onSubmit={props.onSubmit}>
+    <form onSubmit={this._handleDescriptonSubmit}>
       <div className="form-group">
         <div id="des-form">
           <textarea className="form-control btn-block" rows="3"
             placeholder="Write your Description Here"
-            value={props.value}
-            onChange={props.onChange}
+            value={this.state.value}
+            onChange={this._handleDescriptionChange}
           />
           <p>
             <button type="button" className="btn btn-danger">Cancel</button>
@@ -222,10 +184,10 @@ function DescriptionFrom(props) {
           </p>
         </div>
 
-        {props.children}
-
+        {this.props.children}
 
       </div>
     </form>
   );
+}
 }
